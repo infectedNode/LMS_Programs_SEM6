7be31c4a203831f5f3bbd5a5a4537ac85adf6def
Shivam Sharma <shivam231198@gmail.com>
2020-02-11 17:14:38 +0530

    added copyright

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,4 +1,11 @@
-# A try to make this thing possible
+# Copyright (c) 2020 Shivam Sharma
+#
+# Licensed under the MIT License (the "License"); 
+# you may not use this program except in compliance with the License.
+#
+# The above copyright notice and this permission notice shall be 
+# included in all copies or substantial portions of the program.
+#
 
 # Display the board with current position marked
 def displayBoard(status):
