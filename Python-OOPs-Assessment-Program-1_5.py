6a134c7cacbc40dc535424fc96f57ed1e046ee49
RAHUL <rahul.chauhan049@gmail.com>
2020-02-14 22:37:12 +0530

    Update Python-OOPs-Assessment-Program-1_5.py

--- Python-OOPs-Assessment-Program-1_5.py
+++ Python-OOPs-Assessment-Program-1_5.py
@@ -1,19 +1,24 @@
-class bank:
-    def __init__(self):
-        self.balance = 0
-        
-    def deposit(self,amt):
-        self.balance += amt
+class bank: 
+	def __init__(self): 
+		self.balance=0
+		print("Account 'A' Created")
 
-    def withdraw(self,amt):
-        pass   
+	def deposit(self):  
+		amount=int(input("Enter an amount to Deposit\n")) 
+		self.balance += amount 
+		print("Balance= ",amount) 
 
-obj = bank()
+	def withdraw(self): 
+		amount = int(input("Enter an amount to Withdraw\n"))
+		a=int(amount-self.balance)
+		
+		if self.balance>=amount: 
+			self.balance-=amount 
+			print("Balance = ", self.balance) 
+		else:
+			print("Insufficient fund of",a)
+			print("balance", self.balance)
 
-depositAmount = int(input())
-obj.deposit(depositAmount)
-
-withdrawAmount = int(input())
-obj.withdraw(withdrawAmount)
-
-print("100") #don't know y...
+obj1 =bank() 
+obj1.deposit() 
+obj1.withdraw() 
